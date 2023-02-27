1bdf13ffc0225b1477a3184f50151f69ed5a06f1
mozart953 <wilsonnianopop@gmail.com>
2023-02-26 21:33:59 -0600

    cambios

--- Express-1/app.js
+++ Express-1/app.js
@@ -5,4 +5,30 @@ const app= express();
 
 const {infoCursos} =  require('./cursos.js');
 
-console.log(infoCursos);
+// console.log(infoCursos);
+
+app.get('/',(req, res)=>{
+    res.send('servidor express')    
+
+});
+
+app.get('/api/cursos/programacion', (req,res)=>{
+    res.send(JSON.stringify(infoCursos.programacion));
+});
+
+app.get('/api/cursos/matematicas',(req, res)=>{
+    res.send(JSON.stringify(infoCursos.matematicas));
+});
+
+
+app.get('/api/cursos', (req,res)=>{
+    res.send(JSON.stringify(infoCursos));
+});
+
+
+
+const PUERTO = process.env.PUERTO || 3000;
+
+app.listen(PUERTO, ()=>{
+    console.log(`El servidor esta escuchando en el puerto ${PUERTO}`)
+});
