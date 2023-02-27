ba0635403d10a3badbedc30d542b263dd7f6bf98
mozart953 <wilsonnianopop@gmail.com>
2023-02-26 19:15:48 -0600

    pruebas con express

---
+++ Express-1/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app= express();
+
+
+const {infoCursos} =  require('./cursos.js');
+
+console.log(infoCursos);
---
+++ Express-1/cursos.js
@@ -0,0 +1,46 @@
+let infoCursos = {
+    'programacion':[
+        {
+            id:1,
+            titulo: 'Aprende Python',
+            lenguaje: 'python',
+            vistas: 15000,
+            nivel:'basico'
+
+
+        },
+        {
+            id:2,
+            titulo: 'Aprende Python',
+            lenguaje:'python',
+            vistas:15000,
+            nivel: 'intermedio'
+        },
+        {
+            id:3,
+            titulo: 'Aprende Javascript',
+            lenguaje:'python',
+            vistas:102223,
+            nivel: 'basico'
+        }
+    ],
+
+    matematicas:[
+        {
+            id:1,
+            titulo: 'Aprende calculo',
+            tema:'calculo',
+            vistas:12427,
+            nivel: 'basico'
+        },
+        {
+            id:2,
+            titulo: 'Aprende Algebra',
+            tema:'algebra',
+            vistas:15722,
+            nivel: 'intermedio'            
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
